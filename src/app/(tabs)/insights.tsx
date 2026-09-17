3a16a6f82fe2225871577065573acfdfc654796d
Abhishek Raj <abhishekraj995555@gmail.com>
2026-09-17 17:08:49 +0530

    feat: project setup, nativewind, routing and tab navigation

--- src/app/(tabs)/insights.tsx
+++ src/app/(tabs)/insights.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from "react-native";
+import { styled } from "nativewind";
+import { Text } from "react-native";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSafeAreaView);
 
-const insights = () => {
+const Insights = () => {
   return (
-    <View>
-      <Text>insights</Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text>Insights</Text>
+    </SafeAreaView>
   );
 };
 
-export default insights;
+export default Insights;
